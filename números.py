18c22b0470ae6b7f90cd8a93c25c830574ee8dfd
Jhoness Yamit Vaquiro Naranjo <102763320+asher7u7@users.noreply.github.com>
2022-05-06 10:14:07 -0500

    me tocó subirlo así por que por Consola no me dejó

---
+++ números.py
@@ -0,0 +1,15 @@
+#input
+
+x=int(input("digite x: "))
+y=int(input("digite y: "))
+z=int(input("digite z: "))
+
+#procesing and output
+
+s=x+y
+
+if s==z:
+    print("la suma de x + y SI es igual a z")
+else:
+    print("la suma de x + y NO es igual a Z")
+
